95b06c530087363b2a90a7b3fe2a75d90a2171e7
Haidar <diky.hermansyah91@gmail.com>
2026-07-03 06:07:18 +0700

    Force static export - disable SSR completely

--- app/page.js
+++ app/page.js
@@ -1,9 +1,15 @@
-// app/page.js - SUPER SIMPLE WRAPPER
-import ClientOnly from './ClientOnly';
-
+// Force static export
 export const dynamic = 'force-dynamic';
-export const revalidate = 0;
+export const revalidate = false;
+export const ssr = false;
+
+import dynamic from 'next/dynamic';
+
+const LingoSpacePro = dynamic(() => import('../components/LingoSpacePro'), {
+  ssr: false,
+  loading: () => <div>Loading...</div>
+});
 
 export default function Page() {
-  return <ClientOnly />;
+  return <LingoSpacePro />;
 }
